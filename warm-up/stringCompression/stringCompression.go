84e8dc65b05a5bb514c67e7af8e452a8ede102d9
Chris Zietlow <aczietlow@gmail.com>
2023-10-27 08:30:30 -0400

    Fixes some bugs in string compression

--- warm-up/stringCompression/stringCompression.go
+++ warm-up/stringCompression/stringCompression.go
@@ -8,21 +8,25 @@ import (
 func main() {
 	str1 := "aabcccccaaa"
 
-	var newString string
-	count := 1
+	var (
+		newString string
+		prevChar  int32
+	)
 
+	count := 1
 	for key, value := range str1 {
-		if key > 0 && str1[key-1] == str1[key] {
+		if key > 0 && prevChar == value {
 			count++
 		} else {
 			count = 1
 		}
-		if key+1 < len(str1) && str1[key] == str1[key+1] {
+		if key+1 == len(str1) || byte(value) != str1[key+1] {
 			newString += string(value) + strconv.FormatInt(int64(count), 10)
 		}
+		prevChar = value
 	}
 
-	if len(str1) <= len(newString) {
+	if len(newString) < len(str1) {
 		fmt.Println(newString)
 	} else {
 		fmt.Println(str1)

bec7a4253d84f8a486ff99b0e37816449e787cc2
Chris Zietlow <aczietlow@gmail.com>
2023-11-13 08:09:50 -0500

    Added a really basic pager.

--- warm-up/4-GithubCLI/issues.go
+++ warm-up/4-GithubCLI/issues.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"go-practice/warm-up/4-GithubCLI/github"
 	"log"
@@ -8,13 +9,34 @@ import (
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 	result, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d Issues: \n", result.TotalCount)
-	for _, issue := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n", issue.Number, issue.User.Login, issue.Title)
+
+	page := 3
+	for count := 0; count < result.TotalCount; count += page {
+
+		// Don't go out of bounds of the slice.
+		if remaining := result.TotalCount - count; page > remaining {
+			page = remaining
+		}
+		for _, issue := range result.Items[count : count+page] {
+			fmt.Printf("#%-5d %9.9s %.55s\n", issue.Number, issue.User.Login, issue.Title)
+		}
+		if result.TotalCount-count > page {
+			fmt.Println("continue? n")
+			input, _ := reader.ReadString('\n')
+			if input == "n" {
+				continue
+			}
+		}
+
 	}
 
+	//for _, issue := range result.Items {
+	//	fmt.Printf("#%-5d %9.9s %.55s\n", issue.Number, issue.User.Login, issue.Title)
+	//}
 }

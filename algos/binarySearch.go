363973a05c3acaa8a66c071ba3a34373248fe00a
Chris Zietlow <aczietlow@gmail.com>
2025-01-16 20:46:35 -0500

    starting back from scratch at binary search

---
+++ algos/binarySearch.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%v\n", binarySearch([]int{2, 4, 5, 7, 19, 21}, 7))
+	fmt.Printf("%v\n", binarySearch([]int{4, 5, 7, 19, 21}, 7))
+	fmt.Printf("%v\n", binarySearch([]int{1, 3, 5, 7, 9}, 7))
+	fmt.Printf("%v\n", binarySearch([]int{1, 3, 5, 7, 9}, 8))
+}
+
+func binarySearch(list []int, item int) int {
+	low := 0
+	high := len(list)
+	for low <= high {
+		// go will round here, bc mid is a type int
+		mid := (low + high) / 2
+		if item == list[mid] {
+			return mid
+		} else if item < list[mid] {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+
+	}
+	return -1
+}

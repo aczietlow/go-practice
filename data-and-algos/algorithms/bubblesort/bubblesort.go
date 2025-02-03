b1507753ad0dc6c639c6079645f5f07f1dfc1414
Chris Zietlow <aczietlow@gmail.com>
2025-02-02 19:16:45 -0500

    bubble sorttt

---
+++ data-and-algos/algorithms/bubblesort/bubblesort.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(bSort([]int{2, 4, 1, 5, 9, 3, 7, 12, 21, 33, 6}))
+}
+
+func bSort(list []int) []int {
+	for start := 0; start < len(list)-1; start++ {
+		index := start
+		for end := len(list) - 1; end > index; end-- {
+			if list[end] < list[end-1] {
+				temp := list[end-1]
+				list[end-1] = list[end]
+				list[end] = temp
+			}
+		}
+	}
+	return list
+}

800b32b1b19a1183a67b44ca8214affae8826c53
Chris Zietlow <aczietlow@gmail.com>
2025-02-03 18:42:17 -0500

    reworking quicksort to be in-place

--- data-and-algos/algorithms/quicksort/quicksort.go
+++ data-and-algos/algorithms/quicksort/quicksort.go
@@ -5,28 +5,35 @@ import (
 )
 
 func main() {
-	fmt.Println(quicksort([]int{5, 1, 2, 18, 21, 4, 7, 7, 7, 5, 8, 3}))
+	fmt.Println(quickSortStart([]int{5, 1, 2, 18, 21, 4, 7, 7, 7, 5, 8, 3}))
 }
 
-func quicksort(list []int) []int {
-	if len(list) < 2 {
-		return list
-	} else {
-		pivot := list[len(list)/2]
-		left, right, middle := []int{}, []int{}, []int{}
-		for _, value := range list {
-			if value < pivot {
-				left = append(left, value)
-			} else if value > pivot {
-				right = append(right, value)
-			} else {
-				middle = append(middle, value)
-			}
-		}
-		return append(append(quicksort(left), middle...), quicksort(right)...)
+func quickSortStart(list []int) []int {
+	return quickSort(list, 0, len(list)-1)
+}
+
+func quickSort(list []int, low, high int) []int {
+	for low < high {
+		pivot := 0
+		list, pivot := partition(list, low, high)
+		fmt.Println(pivot)
+		fmt.Println(list)
+		list = quickSort(list, low, pivot-1)
+		list = quickSort(list, pivot+1, high)
 	}
+	return list
 }
 
-func partition() {
+func partition(list []int, low, high int) ([]int, int) {
+	pivot := list[high]
+	i := low
+	for j := low; j < high; j++ {
+		if list[j] < pivot {
+			list[i], list[j] = list[j], list[i]
+			i++
+		}
+	}
+	list[i], list[high] = list[high], list[i]
+	return list, i
 
 }

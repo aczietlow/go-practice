fa39b68022478889625a78b8ac2ede6664e21b71
Chris Zietlow <aczietlow@gmail.com>
2026-05-04 06:33:46 -0400

    committing forgotten staged changes

--- warm-up/0-wordFreqCounter/wordFreqCounter.go
+++ warm-up/0-wordFreqCounter/wordFreqCounter.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("test")
 	string1 := "Hello world, hello universe."
-	countWords(string1)
+	fmt.Println(countWords(string1))
 }
 
-func countWords(s string) {
-
+func countWords(s string) int {
+	words := strings.Split(s, " ")
+	return len(words)
 }

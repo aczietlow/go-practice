15a1ad88713eed3c0fbeee582fbe963deafb543f
aczietlow <aczietlow@gmail.com>
2026-06-05 17:03:11 -0400

    firing up the ole brainhole

--- data-and-algos/structures/bst/bst.go
+++ data-and-algos/structures/bst/bst.go
@@ -13,6 +13,8 @@ type Node struct {
 func main() {
 	users := getUsers(18)
 	fmt.Println(users)
+
+	// Creates BST
 	tree := Node{}
 	for _, u := range users {
 		tree.insert(u)
@@ -24,6 +26,30 @@ func main() {
 
 	minUser := tree.getMin()
 	fmt.Printf("Min user: %v\n", minUser)
+
+	fmt.Printf("Deleting user: %v\n", users[13])
+	bst := tree.delete(users[13])
+	// starting with a leaf node for simplification
+	printTree(bst)
+}
+
+func (n *Node) delete(u User) Node {
+	// If the node is empty return, there's nothing to do
+	if n.value == (User{}) {
+		// TODO: should we throw an error at this point? to let the user know they attempted to delete a node that doesnt exist
+		return Node{}
+	}
+
+	// parse down the left tree
+	if u.lt(n.value) {
+		node := n.left.delete(u)
+		n.left = &node
+	} else if u.equal(n.value) { // delete the node?
+		// TODO: this doesn't delete the node, instead inserts an empty node
+		return Node{}
+	}
+
+	return *n
 }
 
 func (n *Node) insert(u User) {
@@ -48,6 +74,7 @@ func (n *Node) insert(u User) {
 			return
 		}
 	}
+
 	// assume gt at this point
 	if n.right == nil {
 		n.right = &Node{
